6dde22a1799a4c94c20e3c5115b8abb6b594d8ab
Maksim Botalov <dev.botalov@gmail.com>
2022-08-25 01:09:47 +0500

    Add UmbrellaModel file

---
+++ Umbrella/Model/UmbrellaModel.swift
@@ -0,0 +1,12 @@
+//
+//  UmbrellaModel.swift
+//  Umbrella
+//
+//  Created by Максим Боталов on 25.08.2022.
+//
+
+import Foundation
+
+protocol UmbrellaModelProtocol {
+    var name: String { get set }
+}
