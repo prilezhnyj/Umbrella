aa222487c8da7e7fc080c9682eac448d2f29af79
Maksim Botalov <dev.botalov@gmail.com>
2022-08-25 01:05:27 +0500

    Initial Commit

---
+++ Umbrella/View/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  Umbrella
+//
+//  Created by Максим Боталов on 23.08.2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        print(#function)
+    }
+}
+
